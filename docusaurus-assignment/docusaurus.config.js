58d4470d4f476d8a98ed9af0e36569e8d6857e7f
Clow <13alemire@gmail.com>
2026-07-22 23:56:22 -0400

    Update docusaurus.config.js
    added a forward slash
    
    yeah that's it

--- docusaurus-assignment/docusaurus.config.js
+++ docusaurus-assignment/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://clowXIV.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-assignment',
+  baseUrl: '/docusaurus-assignment/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
