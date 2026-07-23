3f5cc40b50ef0632c4667485f79073742208ec5b
Clow <13alemire@gmail.com>
2026-07-22 23:44:27 -0400

    Update docusaurus.config.js
    Proper deploy information

--- docusaurus-assignment/docusaurus.config.js
+++ docusaurus-assignment/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://clowXIV.github.io/docusaurus-assignment',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'clowXIV', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-assignment', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
